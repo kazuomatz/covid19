4c01675a529f4fc39bec1fbd2daf093b91e66dc1
hota1024 <hotalog@hota1024.com>
2020-04-23 13:46:10 +0900

    🚨 Fix layouts/print.vue

--- layouts/print.vue
+++ layouts/print.vue
@@ -24,7 +24,9 @@
             <p class="PrintMeta-Text">
               {{ $t('※最新の情報はWebページをご覧ください') }}
             </p>
-            <p class="PrintMeta-Link">https://stopcovid19.city.shizuoka.lg.jp/</p>
+            <p class="PrintMeta-Link">
+              https://stopcovid19.city.shizuoka.lg.jp/
+            </p>
           </div>
         </div>
       </div>
