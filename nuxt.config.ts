463a6b9cb3477a5eff506c473821c4711a3e00c7
hota1024 <hotalog@hota1024.com>
2020-04-23 14:06:04 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,7 +100,8 @@ const config: Configuration = {
     ['nuxt-i18n', i18n],
     'nuxt-svg-loader',
     'nuxt-purgecss',
-    ['vue-scrollto/nuxt', { duration: 1000, offset: -72 }]
+    ['vue-scrollto/nuxt', { duration: 1000, offset: -72 }],
+    '@nuxtjs/axios'
   ],
   /*
    ** vuetify module configuration
