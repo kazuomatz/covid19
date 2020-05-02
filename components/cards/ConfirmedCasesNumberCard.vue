17e1090e8a8c5c2c15864c048a7282621235b773
kazuomatz <g0129matsu>
2020-05-02 18:08:32 +0900

    date update 20200502

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -7,7 +7,7 @@
       :chart-data="patientsGraph"
       :date="Data.patients.date"
       :unit="$t('人')"
-      :url="'https://dataset.city.shizuoka.jp/dataset/1588245187'"
+      :url="'https://dataset.city.shizuoka.jp/dataset/1588408638'"
     />
   </v-col>
 </template>
